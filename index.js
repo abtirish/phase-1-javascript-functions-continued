611a6ba74c05107983a8ca727cd2055c0dc80c01
abtirish <abtirish@gmail.com>
2023-10-11 21:22:53 -0600

    finish phase 1 javascript functions continued lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+ return `This Saturday, I want to ${activity}!`
+}
+console.log(saturdayFun());
+console.log(saturdayFun('bathe my dog'))
+
+
+
+function mondayWork(workOption = 'go to the office') {
+    switch (workOption) {
+        case 'work from home':
+            return 'This Monday, I will work from home.';
+        default:
+            return 'This Monday, I will ' + workOption + '.';
+    }
+};
+
+console.log(mondayWork());
+
+
+
+
+
+
+function wrapAdjective(highlight = "*") {
+    return function(adjective = "special") {
+      return `You are ${highlight}${adjective}${highlight}!`;
+    }
+  }
+
+
+console.log(wrapAdjective()())
+
